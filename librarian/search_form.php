86bfa7e7fa917ef7a8d0803eba4bee75b947e647
nam <nambolo123vn@gmail.com>
2022-12-23 12:16:52 +0700

    them tinh nang tim kiem

---
+++ librarian/search_form.php
@@ -0,0 +1,29 @@
+							<!-- Modal -->
+<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal-header"></div>
+<div class="modal-body">
+    <form class="form-horizontal" method="post" action="advance_search.php">
+				<div class="control-group">
+				<label class="control-label" for="inputEmail">Tên</label>
+				<div class="controls">
+				<input type="text" name="title" id="inputEmail" placeholder="Tên sách">
+				</div>
+				</div>
+					<div class="control-group">
+					<label class="control-label" for="inputPassword">Tác giả</label>
+					<div class="controls">
+					<input type="text" name="author" id="inputPassword" placeholder="tên tác giả">
+					</div>
+					</div>		
+						
+    <div class="control-group">
+    <div class="controls">
+    <button type="submit" class="btn btn-success">Tìm kiếm</button>
+    </div>
+    </div>
+    </form>
+</div>
+<div class="modal-footer">
+<button class="btn" data-dismiss="modal" aria-hidden="true">Tắt</button>
+</div>
+</div>
