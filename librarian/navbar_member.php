6eb47bd66ad20eb77657263adf716fe7f7e184a9
nam <nambolo123vn@gmail.com>
2022-12-25 12:47:29 +0700

    them trang thanh vien

--- librarian/navbar_member.php
+++ librarian/navbar_member.php
@@ -1,4 +1,4 @@
-<div class="navbar navbar-fixed-top navbar-inverse">
+      <div class="navbar navbar-fixed-top navbar-inverse">
             <div class="navbar-inner">
                 <div class="container">
                     <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
@@ -8,14 +8,14 @@
                     </a>
                     <div class="nav-collapse collapse">
 					<ul class="nav">
-					<li class="active"><a href="dashboard.php"><i class="icon-home icon-large"></i>&nbsp;Trang chủ</a></li>
+					<li><a href="dashboard.php"><i class="icon-home icon-large"></i>&nbsp;Trang chủ</a></li>
 					<li><a href="users.php"><i class="icon-user icon-large"></i>&nbsp;Tài khoản</a></li>
-					<?php 
-					include('dropdown.php');
+					<?php
+					 include('dropdown.php');
 					?>
 					<li><a href="books.php"><i class="icon-book icon-large"></i>&nbsp;Sách</a></li>
-					<li><a href="member.php"><i class="icon-group icon-large"></i>&nbsp;Thành viên</a></li>
-					<li><i class="icon-list-alt icon-large"></i>&nbsp;Lưu trữ</a></li>
+					<li class="active"><a href="member.php"><i class="icon-group icon-large"></i>&nbsp;Thành viên</a></li>
+					<li><a href="archive.php"><i class="icon-list-alt icon-large"></i>&nbsp;Lưu trữ</a></li>
 					<li><a href="#myModal" data-toggle="modal"><i class="icon-search icon-large"></i>&nbsp;Tìm kiếm</a></li>
 					<li><a href="logout.php"><i class="icon-signout icon-large"></i>&nbsp;Đăng xuất</a></li>
 					</ul>
@@ -26,6 +26,6 @@
                 </div>
             </div>
         </div>
-		
-<?php include('search_form.php'); ?>
  
+
+			 <?php include('search_form.php'); ?>
