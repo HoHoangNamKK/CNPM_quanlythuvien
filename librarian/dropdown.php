82a9101a3e421848b8ad14404c1fa2c9b766e11d
nam <nambolo123vn@gmail.com>
2022-12-26 13:22:28 +0700

    trang hien sach cho muon

--- librarian/dropdown.php
+++ librarian/dropdown.php
@@ -3,6 +3,6 @@
 					<ul class="dropdown-menu">
 					<li><a href="borrow.php"><i class="icon-pencil icon-large"></i>&nbsp;Mượn</a></li>
 					<li><a href="return.php"><i class="icon-cog icon-large"></i>&nbsp;Xem sách trả</a></li>
-					<li><i class="icon-reorder icon-large"></i>&nbsp;Xem sách mượn</a></li>
+					<li><a href="view_borrow.php"><i class="icon-reorder icon-large"></i>&nbsp;Xem sách mượn</a></li>
 					</ul>
 					</li>
