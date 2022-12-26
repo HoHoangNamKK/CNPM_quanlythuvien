99a22ca1ee5ce72b532f5e97dfed28bb76dfc695
nam <nambolo123vn@gmail.com>
2022-12-26 13:11:53 +0700

    them trang hien sach da tra

--- librarian/dropdown.php
+++ librarian/dropdown.php
@@ -2,7 +2,7 @@
 					<a href="borrow.php"  data-toggle="dropdown" ><i class="icon-file icon-large"></i> Giao dịch</a>
 					<ul class="dropdown-menu">
 					<li><a href="borrow.php"><i class="icon-pencil icon-large"></i>&nbsp;Mượn</a></li>
-					<li><i class="icon-cog icon-large"></i>&nbsp;Xem sách trả</a></li>
+					<li><a href="return.php"><i class="icon-cog icon-large"></i>&nbsp;Xem sách trả</a></li>
 					<li><i class="icon-reorder icon-large"></i>&nbsp;Xem sách mượn</a></li>
 					</ul>
 					</li>
